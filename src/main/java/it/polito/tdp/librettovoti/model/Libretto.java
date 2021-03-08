03234cb3cfc1b30d53f758a2184b22069dca7701
marti <marti@DESKTOP-07IHISP.homenet.telecomitalia.it>
2021-03-08 11:20:00 +0100

    Soluzione punto 2

--- src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
+++ src/main/java/it/polito/tdp/librettovoti/model/Libretto.java
@@ -4,25 +4,54 @@ import java.util.*;
 
 public class Libretto {
 
-	private List <Voto> voti;
-	
+	private List<Voto> voti;
+
 	public Libretto() {
-		this.voti = new ArrayList <>();
-		
+		this.voti = new ArrayList<>();
+
 	}
-	
+
 	public void add(Voto v) {
 		this.voti.add(v);
 	}
 	
-	public String toString() {
-		String s="";
-		for(Voto v: this.voti)
-		{
-			s+= v.toString()+"\n";
-		}
-		return s;
+/*	public String votiUguali (int punteggio) {
+	
+		//calcola una stringa che continene i voti, sarà poi il chiamante a stamparli
 		
+	}*/
+	
+	public List <Voto> listaVotiUguali (int punteggio){
+		//restituisce solo i voti uguali al criterio
+		List <Voto> risultato = new ArrayList <>();
+		for(Voto v: this.voti)
+			if(v.getVoto()==punteggio)
+				risultato.add(v);
+		return risultato;
 	}
 	
+	public Libretto votiUguali(int punteggio) {
+		Libretto risultato = new Libretto();
+		for(Voto v: voti)
+			if(v.getVoto()==punteggio)
+			{
+				risultato.add(v);
+			//	risultato.voti.add(v); --> uguale
+			}
+		return risultato;
+		
+		
+	}
+
+	
+	
+	public String toString() {
+		String s = "";
+		for (Voto v : this.voti) {
+			s += v.toString() + "\n";
+		}
+		return s;
+
+	}
+
 }
